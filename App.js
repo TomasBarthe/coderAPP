41462d6757a2775acbdeebd3d991ee6d2aae5b59
tomas <tomsantiagobarthe@gmail.com>
2023-03-14 18:38:28 -0300

    segunda entrega adeudada

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>HOLA CODERS, MEJOR TARDE QUE NUNCA!</Text>
+      <Text>HOLA CODERS, MEJOR TARDE QUE NUNCA!!</Text>
       <StatusBar style="auto" />
     </View>
   );
